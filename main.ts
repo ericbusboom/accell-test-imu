642a070ddfd2a6e3adc63bfbcf4b3824c0f07ef9
Eric Busboom <eric@civicknowledge.com>
2025-06-27 22:22:05 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,8 +6,55 @@ servos.P1.setAngle(90)
 
 let run = false;
 
+
+// Function to add a new value and calculate the running average
+function addAndAverage(x: number, l: number = 20) {
+    // Add the new value to the end
+    a.push(x);
+
+    if (a.length > l) {
+        a.shift();
+    }
+
+    // Calculate the sum
+    let sum = 0;
+    for (let i = 0; i < a.length; i++) {
+        sum += a[i];
+    }
+
+    // Calculate the average
+    let average = sum / a.length;
+
+    // Return the average
+    return average;
+}
+
 function degToRad(degrees: number): number {
     return degrees * (Math.PI / 180);
+}
+
+function busyWait(durationUs: number, callback: () => void) {
+    const start = control.micros();
+    const intervalUs = 20;
+    let lastCallback = start;
+
+    while (true) {
+        const now = control.micros();
+        const elapsed = now - start;
+
+        if (elapsed >= durationUs) {
+            break;
+        }
+
+        if (now - lastCallback >= intervalUs) {
+            callback();
+            lastCallback = now;
+        }
+
+        // Optionally do a short wait to reduce CPU heating,
+        // but note this introduces jitter:
+        // control.waitMicros(1);
+    }
 }
 
 function mkStepServo(
@@ -22,7 +69,9 @@ function mkStepServo(
         if (start <= end) {
             for (let i = start; i < end; i += step) {
                 servo.setAngle(i);
-                basic.pause(pause);
+                busyWait(pause*1000, function(){
+                    addAndAverage(input.acceleration(Dimension.X))
+                })
                 if (callback) {
                     callback(i);
                 }
@@ -30,7 +79,9 @@ function mkStepServo(
         } else {
             for (let i = start; i > end; i -= step) {
                 servo.setAngle(i);
-                basic.pause(pause);
+                busyWait(pause*1000, function () {
+                    addAndAverage(input.acceleration(Dimension.X))
+                })
                 if (callback) {
                     callback(i);
                 }
@@ -39,8 +90,6 @@ function mkStepServo(
     };
 
 }
-
-
 
 
 input.onButtonPressed(Button.A, function () {
@@ -70,39 +119,18 @@ function makeScaler(
 }
 
 
-// Function to add a new value and calculate the running average
-function addAndAverage(x: number, l: number = 1) {
-    // Add the new value to the end
-    a.push(x);
-
-    if (a.length > l) {
-        a.shift();
-    }
-
-    // Calculate the sum
-    let sum = 0;
-    for (let i = 0; i < a.length; i++) {
-        sum += a[i];
-    }
-
-    // Calculate the average
-    let average = sum / a.length;
-
-    // Return the average
-    return average;
-}
 
 const imuScaler = makeScaler(1024, -1024, -1, 1);
 
+let meanAccel = 0;
 
-const ss = mkStepServo(servos.P1, 2, 30, function(angle: number){
+const ss = mkStepServo(servos.P1, 2, 100, function(angle: number){
 
     // This is actually a 360 degree servo
     angle *= 2
 
-    let m = addAndAverage(input.acceleration(Dimension.Y))
-
-    let v = imuScaler(m);
+    meanAccel = addAndAverage(input.acceleration(Dimension.X))
+    let v = imuScaler(meanAccel);
 
     let a_r = degToRad(angle)
 
@@ -110,6 +138,8 @@ const ss = mkStepServo(servos.P1, 2, 30, function(angle: number){
     serial.writeValue("x", v)
     serial.writeValue("diff", v - Math.cos(a_r))
 })
+
+
 
 basic.forever(function () {
     if (!run) {
